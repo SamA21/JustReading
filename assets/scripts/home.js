f2bfe780fd6776dfbf52677368c577c1a499d5d4
Samaallen@gmail.com <samaallen@gmail.com>
2021-05-02 10:40:31 +0100

    moved scropts to script file

---
+++ assets/scripts/home.js
@@ -0,0 +1,21 @@
+var player = document.getElementById('player').innerText;
+var socket = io();  
+var messages = document.getElementById('messages');
+var form = document.getElementById('form');
+var input = document.getElementById('input');
+
+form.addEventListener('submit', function(e) {
+  e.preventDefault();
+  if (input.value) {
+      input.value = player + " - " + input.value;
+    socket.emit('chat message', input.value);
+    input.value = '';
+  }
+});
+
+socket.on('chat message', function(msg) {
+  var item = document.createElement('li');
+  item.textContent = msg;
+  messages.appendChild(item);
+  window.scrollTo(0, document.body.scrollHeight);
+});
